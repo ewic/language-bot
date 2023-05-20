f41b347a736c1e6aaabac94d85ebd5333e46e895
ewic <ewicexclamationpoint@gmail.com>
2023-05-20 19:53:14 -0400

    Types;

---
+++ src/commands/_index.ts
@@ -0,0 +1,9 @@
+import Hello from "./hello.js";
+import Server from './server.js';
+import User from './user.js';
+
+export default {
+    Hello,
+    Server,
+    User,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,40 @@
-// Require the necessary discord.js classes
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import * as dotenv from 'dotenv';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import commands from './commands/_index.js';
+
+dotenv.config();
+
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+//# region Commands
+console.log(commands);
+//# endregion
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
